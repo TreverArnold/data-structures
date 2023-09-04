e58f19fc8a4b6a079ad3f8d650cae5f81320a1e7
Trever Arnold <treverarnold8@gmail.com>
2023-09-04 14:37:21 -0500

    preliminary work

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# data-structures
+# Classic Data Structures in Python
+
+This repository contains sample code for a collection of classic data structures implemented in Python. (WIP)
+
+## Table of Contents
+
+1. [Introduction](#introduction)
+2. [Data Structures](#data-structures)
+3. [Resources and Collaborations](#resources-and-collaborations)
+4. [License](#license)
+
+## Introduction
+
+Data structures are fundamental components of computer science and software engineering. They are used to organize and manipulate data efficiently. This repository will eventualy contain information regarding several.
+
+## Data Structures
+
+The following data structures are currently implemented in this repository:
+
+- Linked List (WIP)
+
+## Resources and Collaborations
+
+In building these data structure implementations, I found the following resources and collaborations helpful:
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data-structures',
+    version='1.0.0',
+    description='A collection of various data structures',
+    long_description='This package includes a variety of data structures for use in different applications. It provides implementations of commonly used data structures like lists, queues, stacks, trees, and more. The package is designed to be versatile and can be used in a wide range of projects.',
+    author='Trever Arnold',
+    author_email='treverarnold8@gmail.com',
+    url='https://github.com/TreverArnold/data-structures',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='data-structures algorithms',
+)
