dfb2c13f3e97a5a52c718a28448e963c389268c5
Trever Arnold <treverarnold8@gmail.com>
2023-09-21 16:25:10 -0500

    Now uses append rather than push for adding items to que, fixed tests, altered error message and type

--- que_.py
+++ que_.py
@@ -1,26 +1,34 @@
 from doubly_linked_list import DoublyLinkedList
 
+
 class Queue:
     def __init__(self, iterable=None):
-        self.dll = DoublyLinkedList(iterable)
+        self.dll = DoublyLinkedList()
+        self.dll.head = None
+        self.dll.tail = None
+        self.dll._size = 0
+
+        if iterable is not None:
+            for item in iterable:
+                self.dll.append(item)
 
     def enqueue(self, value):
-        self.dll.push(value)
+        self.dll.append(value)
 
     def dequeue(self):
-            try:
-                return self.dll.shift()
-            except ValueError:
-                raise ValueError("Dequeue does not work on an empty queue")
+        try:
+            return self.dll.pop()
+        except ValueError:
+            raise ValueError("Dequeue does not work on an empty queue")
 
     def peek(self):
-        try: 
-            return self.dll.tail.prev.value
+        try:
+            return self.dll.head.next.value
         except AttributeError:
-            raise AttributeError("Peek does not work on an empty queue")
+            raise ValueError("Nothing to peek, que is either 0 or 1 item long")
 
     def size(self):
         return len(self.dll)
-    
+
     def __len__(self):
         return len(self.dll)
--- que_test.py
+++ que_test.py
@@ -3,69 +3,56 @@ import pytest
 from que_ import Queue
 
 
-def test_innit():
+def test_init():
     my_queue = Queue([1, 2, 3])
-    assert my_queue.dll.tail.value == 1
-    assert my_queue.dll.tail.prev.value == 2
-    assert my_queue.dll.head.value == 3
-    assert my_queue.dll._size == 3
+    assert len(my_queue) == 3
+    assert my_queue.dequeue() == 1
+    assert my_queue.dequeue() == 2
+    assert my_queue.dequeue() == 3
+    assert len(my_queue) == 0
 
 
 def test_enqueue():
     my_queue = Queue()
     my_queue.enqueue(1)
-    assert my_queue.dll.tail.value == 1
-    assert my_queue.dll.head.value == 1
-    assert my_queue.dll._size == 1
+    assert len(my_queue) == 1
+    assert my_queue.dequeue() == 1
+    my_queue.enqueue(1)
     my_queue.enqueue(2)
-    assert my_queue.dll.tail.value == 1
-    assert my_queue.dll.head.value == 2
-    assert my_queue.dll._size == 2
+    assert len(my_queue) == 2
+    assert my_queue.dequeue() == 1
+    assert my_queue.dequeue() == 2
+    my_queue.enqueue(1)
+    my_queue.enqueue(2)
     my_queue.enqueue(3)
-    assert my_queue.dll.tail.value == 1
-    assert my_queue.dll.tail.prev.value == 2
-    assert my_queue.dll.head.value == 3
-    assert my_queue.dll._size == 3
+    assert len(my_queue) == 3
+    assert my_queue.dequeue() == 1
+    assert my_queue.dequeue() == 2
+    assert my_queue.dequeue() == 3
 
 
 def test_dequeue():
     my_queue = Queue([1, 2, 3])
-    assert my_queue.dll.tail.value == 1
-    assert my_queue.dll.tail.prev.value == 2
-    assert my_queue.dll.head.value == 3
-    assert my_queue.dll._size == 3 == 3
+    assert len(my_queue) == 3
     assert my_queue.dequeue() == 1
-    assert my_queue.dll._size == 2
-    assert my_queue.dll.tail.value == 2
-    assert my_queue.dll.head.value == 3
+    assert len(my_queue) == 2
     assert my_queue.dequeue() == 2
-    assert my_queue.dll._size == 1
-    assert my_queue.dll.tail.value == 3
-    assert my_queue.dll.head.value == 3
+    assert len(my_queue) == 1
     assert my_queue.dequeue() == 3
-    assert my_queue.dll._size == 0
-    assert my_queue.dll.tail == None
-    assert my_queue.dll.head == None
+    assert len(my_queue) == 0
     with pytest.raises(ValueError, match="Dequeue does not work on an empty queue"):
         my_queue.dequeue()
 
 
 def test_peek():
-    my_queue = Queue([1, 2, 3])
-    assert my_queue.dll.tail.value == 1
-    assert my_queue.dll.tail.prev.value == 2
-    assert my_queue.dll.head.value == 3
-    assert my_queue.dll._size == 3
+    my_queue = Queue([1, 2, 3, 4])
     assert my_queue.peek() == 2
-    assert my_queue.dll.tail.value == 1
-    assert my_queue.dll.tail.prev.value == 2
-    assert my_queue.dll.head.value == 3
-    assert my_queue.dll._size == 3
+    assert len(my_queue) == 4
     my_queue = Queue()
-    with pytest.raises(AttributeError, match="Peek does not work on an empty queue"):
+    with pytest.raises(
+        ValueError, match="Nothing to peek, que is either 0 or 1 item long"
+    ):
         my_queue.peek()
-    assert my_queue.dll.tail == None
-    assert my_queue.dll.head == None
     assert my_queue.dll._size == 0
 
 
