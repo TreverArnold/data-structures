0fc730648d192e6d7045c3bb2fcf1057de79adee
Trever Arnold <treverarnold8@gmail.com>
2023-09-21 13:52:19 -0500

    removed one method, switched to try except blocks

--- que_.py
+++ que_.py
@@ -8,20 +8,19 @@ class Queue:
         self.dll.push(value)
 
     def dequeue(self):
-        if len(self.dll) == 0:
-            raise ValueError("Dequeue does not work on an empty queue")
-        return self.dll.shift()
+            try:
+                return self.dll.shift()
+            except:
+                raise ValueError("Dequeue does not work on an empty queue")
 
     def peek(self):
-        if len(self.dll) == 0:
+        try: 
+            return self.dll.tail.prev.value
+        except:
             raise ValueError("Peek does not work on an empty queue")
-        return self.dll.tail.prev.value
 
     def size(self):
         return len(self.dll)
     
     def __len__(self):
         return len(self.dll)
-
-    def is_empty(self):
-        return len(self.dll) == 0
